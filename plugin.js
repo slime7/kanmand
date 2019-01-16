5d4b07fa915496252b16fbc03a61cd06553ddb13
Slime7 <slime7@foxmail.com>
2019-01-16 16:38:23 +0800

    尝试将poi插件打包进软件

---
+++ plugin.js
@@ -0,0 +1,15 @@
+const npm = require('npm');
+const path = require('path');
+const fs = require('fs');
+
+npm.load(() => {
+  npm.commands.pack(['./poi-ghost'], (err, data) => {
+    const rootdir = path.join(__dirname);
+    const packagename = data[0].filename;
+    fs.rename(path.join(rootdir, packagename),
+      path.join(rootdir, 'public', 'poi-plugin-ghost.tgz'),
+      (fserr) => {
+        console.log(fserr.message);
+      });
+  });
+});
